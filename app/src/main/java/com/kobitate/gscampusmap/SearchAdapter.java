6e905ce49415e43e57ef201b91649e2a93d43754
Kobi Tate <kobi@kobitate.com>
2017-02-15 14:47:16 -0500

    Removed accidental import

--- app/src/main/java/com/kobitate/gscampusmap/SearchAdapter.java
+++ app/src/main/java/com/kobitate/gscampusmap/SearchAdapter.java
@@ -8,8 +8,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.google.android.gms.vision.text.Text;
-
 import java.util.List;
 
 /**

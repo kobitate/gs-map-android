47ca6cb8bd0417a1b165f90b58dbbb4c6757c589
Kobi Tate <kobi@kobitate.com>
2017-06-03 15:06:28 -0400

    Fixed a Location Permission crash

--- app/src/main/java/com/kobitate/gscampusmap/MapsActivity.java
+++ app/src/main/java/com/kobitate/gscampusmap/MapsActivity.java
@@ -227,8 +227,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	@Override
 	protected void onResume() {
 		super.onResume();
-		if (didStart) {
-			setupLocationListener();
+		if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+			if (didStart) {
+				setupLocationListener();
+			}
 		}
 	}
 
